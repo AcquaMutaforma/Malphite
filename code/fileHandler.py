647190c5eae1c14c90aba8995db2dde8315e332c
Urolit <aley.pallotta@gmail.com>
2022-05-20 17:55:10 +0200

    Codice di base per registratore e fileHandler
    Ora il registratore funziona, forse ha bisogno di qualche perfezionamento. FileHandler ho aggiunto i metodi che avevamo previsto e ho scritto il primo necessario per i test del registratore + traduttore.

--- code/fileHandler.py
+++ code/fileHandler.py
@@ -1,17 +1,58 @@
-percorso_cartella = ""
+import random
+from scipy.io.wavfile import write
+
+cartella_comando = "audio_richieste/"
 
 
-def get_lista_registrazioni():
-    """Restituisce una lista dei file audio risposta da riprodurre """
-    pass
+def crea_file_richiesta(freq, recording):
+    """Questo metodo scrive la registrazione nella cartella_comando, se e' gia' presente un file
+    con lo stesso nome lo sovrascrive. Tecnicamente i comandi vengono gestiti singolarmente, ma nel
+    dubbio i file vengono chiamati in maniera differente con un numero random. """
+    file_audio = cartella_comando + "richiesta" + str(random.randrange(0, 20)) + ".wav"
+    try:
+        write(file_audio, freq, recording)
+    except PermissionError:
+        print(f"[File_H] - Errore permessi scrittura file in ^ {cartella_comando} ^")
+        return None
+    except FileNotFoundError:
+        print(f"[File_H] - Errore file not found - {cartella_comando} -or- {recording.__class__}")
+        return None
+    except Exception:
+        print("[File_H] - Errore indefinito - crea_file_richiesta()")
+        return None
+    return file_audio
 
 
-def add_registrazione(registrazione):
-    """Inserisce una nuova registrazione nella cartella di risposte registrate dal custode"""
-    pass
+def elimina_audio_processato(filename):
+    pass  # todo
 
 
-def get_registrazione(nome_file):
+def get_mappa_decisore(todo):
+    pass  # todo
+
+
+def update_mappa_decisore(todo):
+    pass  # todo
+
+
+def get_audio_risposta(nome_file):
     """Recupera la registrazione di una risposta, se non viene trovata genera un errore"""
     pass
 
+
+def add_audio_risposta(registrazione):
+    """Inserisce una nuova registrazione nella cartella di risposte registrate dal custode"""
+    pass  # todo
+
+
+'''
+Le funzioni seguenti sono secondarie, da fare con calma
+'''
+
+
+def update_audio_risposta(todo):
+    pass  # todo
+
+
+def elimina_audio_risposta(todo):
+    pass  # todo
---
+++ code/registratore.py
@@ -0,0 +1,48 @@
+""" Classe che gestisce l'input audio
+Responsabilita' :
+Catturare audio in input
+Definire durata dell'audio
+"""
+
+import sounddevice as sd
+import fileHandler
+
+"""
+For repeated use you can set defaults using default:
+sd.default.samplerate = frequency
+sd.default.channels = 2
+
+After that, you can drop the additional arguments:
+myrecording = sd.rec(int(duration * frequency))
+
+"""
+frequency = 44400  # Sampling frequency
+sd.default.samplerate = frequency
+sd.default.channels = 2
+
+
+def get_audio(secondi):
+    if secondi is None:
+        duration = 4.0  # Recording duration in seconds
+    else:
+        duration = secondi
+    # to record audio from sound-device into a Numpy
+    # recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=2)
+    print("[Registratore] - avvio registrazione ")
+    recording = sd.rec(int(duration * frequency))
+    sd.wait()  # Wait for the audio to complete
+    print("[Registratore] - registrazione completata!")
+
+    # using scipy to save the recording in .wav format
+    # This will convert the NumPy array to an audio file with the given sampling frequency
+    nome_file = __audio_to_file(recording=recording)
+    print(f"[audio_H] - Registrazione = {nome_file}")
+    return nome_file  # Restituisco il nome, poi il traduttore lo usa.
+    # In alternativa possiamo chiamare da qua il traduttore, da valutare todo..
+
+
+def __audio_to_file(recording):
+    return fileHandler.crea_file_richiesta(frequency, recording)
+
+
+#  get_audio(None)
